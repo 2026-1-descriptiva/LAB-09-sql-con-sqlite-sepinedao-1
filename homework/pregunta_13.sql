a66f74a7d06437fd1f8e526cddd4c91f968ea458
sepinedao <sepinedao@unal.edu.co>
2026-04-26 20:20:11 -0500

    solucion_lab_09

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -42,4 +42,10 @@
 --  4  E  474.825000
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
+SELECT K0, ROUND(avg(c12) + 1E-10, 2) AS 'avg(c12)'
+FROM tbl1
+WHERE c13 > 400
+GROUP BY K0
+ORDER BY K0;
+
 --  
--- homework/pregunta_14.sql
+++ homework/pregunta_14.sql
@@ -42,5 +42,10 @@
 --  4  E  555.323077
 --
 --  >>> Escriba su codigo a partir de este punto <<<
+SELECT t1.K0, AVG(t2.c21) AS 'avg(c21)'
+FROM tbl1 t1
+JOIN tbl2 t2 on t1.K1 = t2.K1
+WHERE t1.c13 > 400
+GROUP BY t1.K0;
 --  
 
